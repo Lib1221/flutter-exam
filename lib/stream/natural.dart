fcc691858802a5bd6869f2d369c33a27253763c1
Liben <adugnaliben65@gmail.com>
2024-12-05 22:52:30 +0300

    modeling and classify question by grade year and course

--- lib/stream/natural.dart
+++ lib/stream/natural.dart
@@ -1,4 +1,4 @@
-import 'package:exam_store/main_page/screens/quiz_screen.dart';
+import 'package:exam_store/list_Question/listQ.dart';
 import 'package:flutter/material.dart';
 
 class NaturalSubjects extends StatelessWidget {
@@ -17,125 +17,38 @@ class NaturalSubjects extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            ElevatedButton(
-                  onPressed: () {
-                    String collectionPath = 'Biology'; 
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => QuizScreen(collectionPath: collectionPath),
-                  ),
-            );
-              },
-              style: ElevatedButton.styleFrom(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                padding: const EdgeInsets.all(20),
-              ),
-              child: const Text("BIOLOGY"),
-            ),
-
-
-            ElevatedButton(
-                onPressed: () {
-                    String collectionPath = 'Chemistry'; 
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => QuizScreen(collectionPath: collectionPath),
-                  ),
-            );
-              },
-              style: ElevatedButton.styleFrom(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                padding: const EdgeInsets.all(20),
-              ),
-              child: const Text("CHEMISTRY"),
-            ),
-            ElevatedButton(
-               onPressed: () {
-                    String collectionPath = 'English'; 
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => QuizScreen(collectionPath: collectionPath),
-                  ),
-            );
-              },
-              style: ElevatedButton.styleFrom(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                padding: const EdgeInsets.all(20),
-              ),
-              child: const Text("ENGLISH"),
-            ),
-            ElevatedButton(
-               onPressed: () {
-                    String collectionPath = 'Sat'; 
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => QuizScreen(collectionPath: collectionPath),
-                  ),
-            );
-              },
-              style: ElevatedButton.styleFrom(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                padding: const EdgeInsets.all(20),
-              ),
-              child: const Text("SAT"),
-            ),
-            ElevatedButton(
-               onPressed: () {
-                    String collectionPath = 'Mathematics'; 
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => QuizScreen(collectionPath: collectionPath),
-                  ),
-            );
-              },
-              style: ElevatedButton.styleFrom(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                padding: const EdgeInsets.all(20),
-              ),
-              child: const Text("MATHEMATICS"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                    String collectionPath = 'Physics'; 
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => QuizScreen(collectionPath: collectionPath),
-                  ),
-            );
-              },
-              style: ElevatedButton.styleFrom(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                padding: const EdgeInsets.all(20),
-              ),
-              child: const Text("PHYSICS"),
-            ),
+            course(context, 'Biology'),
+            course(context, 'Physics'),
+            course(context, 'Sat'),
+            course(context, 'Mathematics'),
+            course(context, 'Chemistry'),
+            course(context, 'English'),
           ],
         ),
       ),
     );
   }
+}
+
+Widget course(BuildContext context, String collectionPath) {
+  return ElevatedButton(
+    onPressed: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ListQuestions(
+            collectionPath: collectionPath,
+          ),
+        ),
+      );
+    },
+    style: ElevatedButton.styleFrom(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      padding: const EdgeInsets.all(20),
+    ),
+    child: Text(collectionPath),
+  );
 }
